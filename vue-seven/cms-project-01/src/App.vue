d64f948d05616df5e31a6e93a1cab9c242a16732
jonsonice <jonsonices@gmail.com>
2019-03-12 10:18:44 +0800

    头部底部栏添加

--- vue-seven/cms-project-01/src/App.vue
+++ vue-seven/cms-project-01/src/App.vue
@@ -1,23 +1,51 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- 顶部栏区域 -->
+    <mt-header title="信息管理">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <!-- 路由组件的出口 -->
     <router-view/>
+
+    <!-- 底部栏 -->
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="index">
+        <img slot="icon" src="./assets/images/sy.png">
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="vip">
+        <img slot="icon" src="./assets/images/hy.png">
+        会员
+      </mt-tab-item>
+      <mt-tab-item id="shopcat">
+        <img slot="icon" src="./assets/images/gwc.png">
+        购物车
+      </mt-tab-item>
+      <mt-tab-item id="search">
+        <img slot="icon" src="./assets/images/cz.png">
+        查找
+      </mt-tab-item>
+    </mt-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      selected:''
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    padding: 0;
+    margin: 0;
+  }
 </style>
